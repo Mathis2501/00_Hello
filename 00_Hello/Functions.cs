03bdde1394828d0c4b3adb966d79f521c4c8ef70
Mathias Bellerby <Mathis2501@gmail.com>
2016-09-05 13:30:53 +0200

    Begge tests pass

---
+++ 00_Hello/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Hello
+{
+    class Functions
+    {
+
+        public static String Hello()
+        {
+            return ("hello");
+
+        }
+        public static String Hello(String v)
+        {
+            return ("hello, " + v);
+        }
+    }
+}
